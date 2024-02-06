e194d8ca3d05d5b1ba0c4370565679e4c5f1a3a0
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2024-02-06 09:11:09 -0500

    Creación de variables
    Creación de variables para el programa

--- app/src/main/java/com/prueba/programacionmovil_clase1/MainActivity.java
+++ app/src/main/java/com/prueba/programacionmovil_clase1/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean result = true;
+    String name = "Bob";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
