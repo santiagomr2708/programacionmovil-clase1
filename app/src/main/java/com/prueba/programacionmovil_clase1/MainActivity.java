e78c4cf3c1a2f69b73c691fa0fd7acf692ca17f2
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2024-02-08 09:30:29 -0500

    actualización app de prueba
    se trabajó en la interfaz grafica trabajando en colores, texto, botones y sus funciones

--- app/src/main/java/com/prueba/programacionmovil_clase1/MainActivity.java
+++ app/src/main/java/com/prueba/programacionmovil_clase1/MainActivity.java
@@ -5,11 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    boolean result = true;
-    String name = "Bob";
+    TextView nombreUsuario;
+    Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombreUsuario=(TextView) findViewById(R.id.usuario);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                nombreUsuario.setText("Presioné boton Uno");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                nombreUsuario.setText("Presioné boton Dos");
+            }
+        });
+    }
+
+    public double calcularArea(int l1, int l2){
+        return l1*l2;
     }
 }
