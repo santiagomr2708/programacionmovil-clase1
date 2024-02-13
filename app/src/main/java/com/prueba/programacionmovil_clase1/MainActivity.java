e8585ecfe1c9b9c2b6a382e040f24396374a050b
santiagomr2708 <113460466+santiagomr2708@users.noreply.github.com>
2024-02-13 09:11:11 -0500

    intento corrección de errores de clase 2

--- app/src/main/java/com/prueba/programacionmovil_clase1/MainActivity.java
+++ app/src/main/java/com/prueba/programacionmovil_clase1/MainActivity.java
@@ -1,40 +1,47 @@
-package com.prueba.programacionmovil_clase1;
+package co.nelson.appuno;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView nombreUsuario;
-    Button btn1,btn2;
+
+   TextView nombreUsuario;
+   Button btn1,btn2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nombreUsuario=(TextView) findViewById(R.id.usuario);
-        btn1=findViewById(R.id.btn1);
-        btn2=findViewById(R.id.btn2);
+        nombreUsuario = (TextView) findViewById(R.id.usuario);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
 
 
         btn1.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View view){
-                nombreUsuario.setText("Presioné boton Uno");
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Uno");
+
             }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View view){
-                nombreUsuario.setText("Presioné boton Dos");
+            public void onClick(View view) {
+                nombreUsuario.setText("Presioné Boton Dos");
             }
         });
+
+
     }
 
-    public double calcularArea(int l1, int l2){
+    public double calcularArea(int l1,int l2){
         return l1*l2;
     }
+
 }
